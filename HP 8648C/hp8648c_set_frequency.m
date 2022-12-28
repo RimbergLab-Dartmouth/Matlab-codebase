5416badaddcc4afcd0657571fe26e0288d261fdb
Bhargava Thyagarajan <45409742+bhargava-thyagarajan@users.noreply.github.com>
2022-12-28 11:50:41 -0500

    Delete HP 8648C directory

--- HP 8648C/hp8648c_set_frequency.m
+++
@@ -1,3 +0,0 @@
-function []=hp8648c_set_frequency(sg_handle,freq)
-   fprintf(sg_handle,[':freq ' num2str(freq)]);
-end
--- HP 8648C/hp8648c_toggle_output.m
+++
@@ -1,3 +0,0 @@
-function[]=hp8648c_toggle_output(sig_gen_handle,on_off)
-     fprintf(sig_gen_handle,[':outp ' on_off]);
-end
