2e1ff05b1ad2fd144763666360d2c085304ab733
Bhargava Thyagarajan <bhargava.thyagarajan.gr@dartmouth.edu>
2023-01-06 14:04:22 -0500

    A

--- Procedures/switching_rate_finder_single_cCPT_setting.m
+++ Procedures/switching_rate_finder_single_cCPT_setting.m
@@ -779,12 +779,13 @@ if ~run_params.analysis_during_acquisition
         if ~exist('m_save_data_counter', 'var')
             disp('doesnt exist')
             m_save_data_counter = 1;
+            m_record = 1;
         end
     end
-    raw_data_matrix.time(m_save_data_counter, :) = raw_data.time;
+%     raw_data_matrix.time(m_save_data_counter, :) = raw_data.time;
     raw_data_matrix.voltage(m_save_data_counter, :) = raw_data.voltage;
-    raw_data_matrix.amp_extracted(m_save_data_counter, :) = raw_data.amp_extracted;
-    raw_data_matrix.phase_extracted(m_save_data_counter, :) = raw_data.phase_extracted;
+%     raw_data_matrix.amp_extracted(m_save_data_counter, :) = raw_data.amp_extracted;
+%     raw_data_matrix.phase_extracted(m_save_data_counter, :) = raw_data.phase_extracted;
     clear raw_data
 end
 clear size_required
@@ -804,14 +805,16 @@ if (detuning_point > run_params.detuning_point_end + run_params.detuning_point_s
 end
 %% save only raw data matrix struct for further analysis later 
 if run_params.save_data_and_png_param == 1 && ~run_params.analysis_during_acquisition
-    if m_save_data_counter == input_params.save_raw_data_frequency
+    if m_save_data_counter == input_params.save_raw_data_frequency ||((detuning_point > run_params.detuning_point_end + run_params.detuning_point_step ...
+            || detuning_point == run_params.detuning_point_end) && m_repetition == run_params.number_repetitions)    
         save([run_params.data_directory '\' num2str(m_power) '_' num2str(m_flux) '_' num2str(m_gate) '_' ...
-            num2str(m_repetition) 'raw_data_record_' num2str(m_save_data_counter)], 'raw_data.mat', 'raw_data_matrix')   
+            'raw_data_record_' num2str(m_record) '_raw_data.mat'], 'raw_data_matrix')   
         m_save_data_counter = 0;
         clear raw_data_matrix
         disp('saved raw data')
+        m_record = m_record + 1;
     end
-    m_save_data_counter = m_save_data_counter + 1
+    m_save_data_counter = m_save_data_counter + 1;
 end
 %% clear some unrequired variables that will be reloaded next iteration of this function
 clear vna_data_acquisition ...
