f8744884b46e4b40607bc4ea1bc33c650c90f510
Bhargava Thyagarajan <bhargava.thyagarajan.gr@dartmouth.edu>
2023-02-17 17:29:04 -0500

    Chris uncommitted changes

--- Procedures/Chris_Feb12_Error_Signal_From_Sample.m
+++ Procedures/Chris_Feb12_Error_Signal_From_Sample.m
@@ -1,25 +1,27 @@
 clc;
 clear_workspace_connect_instruments;
-switch_PDH_measurement(keysight_sg)
+switch_PDH_measurement(keysight_sg, ps_2)
 n5183b_toggle_output(keysight_sg,'on')
-n5183b_set_amplitude(keysight_sg,-40)
-n5183b_set_frequency(keysight_sg, 5.783e9)
+n5183b_set_amplitude(keysight_sg,-25)
+n5183b_set_frequency(keysight_sg, 5.7845e9)
 novatech_set_phase(novatech,0,0);
 novatech_set_freq(novatech,30,0);
 novatech_set_phase(novatech,0,1);
 novatech_set_freq(novatech,30,1);
 
-range = 15;
-freq = -range:range;
+centerFreq = 5.7845e9;
+range = 5;
+resolution = 0.1;%MHz
+freq = -range:resolution:range;
 record = zeros(1,length(freq));
 rep = 100;
 
 disp('rough estimate:')
-disp(2*range*(1.5+rep*0.5)/60)
+disp(2*range/resolution*(1.5+rep*0.5)/60)
 disp('minutes')
 
 for i = 1:length(freq)
-    n5183b_set_frequency(keysight_sg, 5.783e9+freq(i)*1e6)
+    n5183b_set_frequency(keysight_sg, centerFreq+freq(i)*1e6)
     pause(1.5);
     X = abs(sr844_lockin_query_measured_value(lockin_sr844,'X'));
     Y = abs(sr844_lockin_query_measured_value(lockin_sr844,'Y'));
--- utilities/switch_PDH_measurement.m
+++ utilities/switch_PDH_measurement.m
@@ -1,12 +1,12 @@
 function [] = switch_PDH_measurement(keysight_sg, ps_2)
-    if exist('ps_2', 'var')
-        hp_6612c_set_voltage(ps_2,10,'on');
-    end
+    
+    hp_6612c_set_voltage(ps_2,10,'on');
     n5183b_set_amplitude(keysight_sg, -65)
     two_way_switch_control('A', 1)
     two_way_switch_control('B', 1)
     two_way_switch_control('C', 1)
     two_way_switch_control('D', 1)
 	four_way_switch_control(4);
-    disp('switched to PDH setup')
+    disp('switched to PDH setup')   
+    
 end
