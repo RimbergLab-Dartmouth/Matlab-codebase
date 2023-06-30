84e58896173ffa1f806d1d4e0840d505c2ec567f
Bhargava Thyagarajan <bhargava.thyagarajan.gr@dartmouth.edu>
2023-06-30 12:25:55 -0400

    A

--- Procedures/monitor_res_freq_over_time.m
+++ Procedures/monitor_res_freq_over_time.m
@@ -1,6 +1,6 @@
 %% run parameters
 input_params.flux_bias = 0;
-input_params.gate_bias = 0;
+input_params.gate_bias = 0.5;
 input_params.monitoring_time_spacing = 10; % in seconds
 input_params.min_finder.channel_number = 1;
 input_params.min_finder.trace_number = 1;
@@ -27,7 +27,7 @@ set_bias_point_using_offset_period_struct(input_params.gate_bias, input_params.f
 %%% generate folder name
 if input_params.save_data_and_png_param == 1 || input_params.save_fig_param == 1
     input_params.file_name_time_stamp = datestr(now, 'yymmdd_HHMMSS');
-    run_params.file_name = ['_res_freq_drift' num2str(input_params.gate_bias*100) '_flux_0p' num2str(input_params.flux_bias*100)];
+    input_params.file_name = ['_res_freq_drift' num2str(input_params.gate_bias*100) '_flux_0p' num2str(input_params.flux_bias*100)];
 end
 %%% make necessary folders
 if input_params.save_data_and_png_param 
@@ -46,7 +46,8 @@ run_params.i = 1;
 run_params.loop_running = true;
 run_params.last_save_time = 0;
 tic
- while run_params.loop_running
+figure
+while run_params.loop_running
     pause(input_params.monitoring_time_spacing)
     %%%% set one marker at the initial res freq
     if run_params.i == 1
@@ -55,7 +56,7 @@ tic
          vna_set_marker_freq(vna,input_params.initial_min_set_marker,data.initial_res_freq,input_params.min_finder.channel_number)
          vna_set_center_span(vna, data.initial_res_freq, input_params.vna.zoom_span, input_params.min_finder.channel_number);
          vna_set_IF_BW(vna, input_params.vna.zoom_IF_BW, input_params.min_finder.channel_number);
-         vna_set_average(vna, input_params.vna.zoom_average-number, input_params.min_finder.channel_number);
+         vna_set_average(vna, input_params.vna.zoom_average_number, input_params.min_finder.channel_number);
          vna_set_sweep_points(vna, input_params.vna.zoom_number_points, input_params.min_finder.channel_number);
     end
     data.res_freq_array(run_params.i) = vna_get_marker_data(vna,input_params.min_finder.marker_number,...
@@ -68,4 +69,4 @@ tic
         save([input_params.data_directory input_params.file_name '.mat'], '-regexp', '^(?!(run_params)$).')
     end
     run_params.i = run_params.i + 1;
- end
+end
