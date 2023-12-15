36e95e924ce678a427f8823c0600a8dbac5bcd03
Chris Wang <qianxu.wang.gr@dartmouth.edu>
2023-12-15 16:37:46 -0500

    radius correction made for q-circle with angle

--- Analysis/q_circle.m
+++ Analysis/q_circle.m
@@ -1,7 +1,6 @@
 function [gamma_real,gamma_imag]=q_circle(freq_points,gamma_int,gamma_ext,freq_res)
-%      gamma_real= ((freq_points-freq_res).^2+gamma_int^2-gamma_ext^2)./((freq_points-freq_res).^2+(gamma_int+gamma_ext)^2);
-%      gamma_imag=2*(freq_points-freq_res)*gamma_ext./((freq_points-freq_res).^2+(gamma_int+gamma_ext)^2);
-     gamma = ((freq_points - freq_res)-1i.*(gamma_int - gamma_ext)/2)./((freq_points - freq_res) - 1i*(gamma_int + gamma_ext)/2);
-     gamma_real = real(gamma);
-     gamma_imag = imag(gamma);
+    % Bhar eq 2.31
+    gamma = ((freq_points - freq_res)-1i.*(gamma_int - gamma_ext)/2)./((freq_points - freq_res) - 1i*(gamma_int + gamma_ext)/2);
+    gamma_real = real(gamma);
+    gamma_imag = imag(gamma);
 end
--- Analysis/q_circle_with_freq_flucs_and_angle.m
+++ Analysis/q_circle_with_freq_flucs_and_angle.m
@@ -1,11 +1,15 @@
 function [gamma_real,gamma_imag]=q_circle_with_freq_flucs_and_angle(freq_points,gamma_int,gamma_ext,freq_res, sigma, angle_degs)
-     angle_radians = 2*pi/360*angle_degs;
-     gamma = 1 - sqrt(pi/2).*gamma_ext./sigma.*fadf((1i*(gamma_int+gamma_ext) - 2*(freq_points - freq_res))./(2*sqrt(2).*sigma));
-     gamma = gamma - 1;
-     gamma = gamma.*exp(-1i*angle_radians);
-     gamma = gamma +1;
-     gamma_real = real(gamma);
-     gamma_imag = imag(gamma);
-%      gamma_real= ((freq_points-freq_res).^2+gamma_int^2-gamma_ext^2)./((freq_points-freq_res).^2+(gamma_int+gamma_ext)^2);
-%      gamma_imag=2*(freq_points-freq_res)*gamma_ext./((freq_points-freq_res).^2+(gamma_int+gamma_ext)^2);
+% Bhar eq 3.7 + rotation
+    angle_radians = 2*pi/360*angle_degs;
+    gamma = 1 - sqrt(pi/2).*gamma_ext./sigma.*fadf((1i*(gamma_int+gamma_ext) - 2*(freq_points - freq_res))./(2*sqrt(2).*sigma));
+    gamma = gamma - 1;
+
+
+    % account for radius change
+    % gamma = gamma.*exp(-1i*angle_radians);
+    gamma = gamma.*exp(-1i*angle_radians).*cos(angle_radians);
+
+    gamma = gamma +1;
+    gamma_real = real(gamma);
+    gamma_imag = imag(gamma);
 end
